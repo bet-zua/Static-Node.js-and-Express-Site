db7227ba42c5ce2fc10c7ae2563a519dacc38299
Bet-Zua Jimenez <betzuaj@gmail.com>
2021-04-21 15:48:20 -0500

    Finalizes functionality

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # Static-Node.js-and-Express-Site
-Techdegree portfolio website. 
+
+Portfolio site containing a landing page, about page, and project pages featuring the first five projects from the Team Treehouse Techdegree. 
+
+The site is running on Node.js + Express, and the markup is generated from Pug templates which utilize a JSON file containing project information.
+
+Additional Features:
+* You can run the app by running 'npm start'
+* Customized error message Pug templates which display the error's message, status, and stack. Templates link back to the home page.
+* Customized text colors, background color & box shadows
--- app.js
+++ app.js
@@ -1,32 +1,32 @@
+/**
+ * Require necessary dependencies
+*/
 const express = require('express');
-const { projects }  = require('./data.json'); // this must not be an object bc projects in an array
+const { projects }  = require('./data.json'); 
 const app = express();
 
+/**
+ * Middleware
+*/
 app.set('view engine', 'pug');
-// serve static files from public folder
-app.use('/static', express.static('public'));
+app.use('/static', express.static('public')); // serve static files from public folder
+
 
 /**
  * Routes
 */
-// index route to render Home page
 app.get('/', (req, res)=>{
     res.render('index', { projects });
 });
-// render About page
+
 app.get('/about', (req, res, next)=>{
     res.render('about');
-    // const err = new Error();
-    // err.status = 500;
-    // err.message = "Unexpected server error!";
-    // next(err);
 });
-// render dynamic Project routes
+
 app.get('/projects/:id', (req, res, next)=>{
-    const id = req.params.id; // adding data as an object that contains data to be passed to Pug template
+    const id = req.params.id; 
     const project = projects[id];
     if (project) {
-        // console.log('project page loaded');
         res.locals.data = projects;
         return res.render('project', { project });
     } else {
@@ -51,13 +51,12 @@ app.use((req, res, next)=>{
     res.render('page-not-found', { err });
 });
 app.use((err, req, res, next)=>{
-    console.log('500: Unexpected server error.');
-
     if (err.status === 404 ){
+        console.log('404: This page does not exist, oops!');
         res.status(404);
         res.render('page-not-found', { err });
     } else {
-        // set status and message if they do not exist
+        console.log('500: Unexpected server error.');
         err.message = err.message || 'Unexpected server error.';
         res.status(err.status || 500);
         res.render('error', { err });
